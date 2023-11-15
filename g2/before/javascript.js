6968626d693242b118880a62ea1ea18c0f50e38c
109B29689 <91858061+109B29689@users.noreply.github.com>
2023-11-15 22:11:11 +0800

    Update javascript.js

--- g2/before/javascript.js
+++ g2/before/javascript.js
@@ -1,3 +1,4 @@
+/*
 const fillDivs = document.querySelectorAll('.fill');
 const emptyDivs = document.querySelectorAll('.puzzle:not(.fill)');
 
@@ -43,4 +44,47 @@ function drop() {
     if (this.classList.contains('fill')) return;
     this.append(draggedDiv);
     this.classList.remove('hovered');
+}
+*/
+$(document).ready(function() {
+  $('.fill').on('dragstart', dragStart);
+  $('.fill').on('dragend', dragEnd);
+  $('.puzzle:not(.fill)').on('dragover', dragOver);
+  $('.puzzle:not(.fill)').on('dragenter', dragEnter);
+  $('.puzzle:not(.fill)').on('dragleave', dragLeave);
+  $('.puzzle:not(.fill)').on('drop', drop);
+});
+
+function dragStart(event) {
+  const holdTimeout = setTimeout(function() {
+    $(this).addClass('hold');
+  }, 0);
+  draggedDiv = this;
+  return false;
+}
+
+function dragEnd(event) {
+  draggedDiv = null;
+  $(this).removeClass('hold');
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function dragEnter(event) {
+  event.preventDefault();
+  $(this).addClass('hovered');
+}
+
+function dragLeave(event) {
+  $(this).removeClass('hovered');
+}
+
+function drop(event) {
+  if ($(this).hasClass('fill')) {
+    return;
+  }
+  $(this).append(draggedDiv);
+  $(this).removeClass('hovered');
 }
